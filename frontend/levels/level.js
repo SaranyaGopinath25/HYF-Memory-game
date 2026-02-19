58379287542d6c22ade3abf517fb4b8290569db0
Saranya Gopinath <142643776+SaranyaGopinath25@users.noreply.github.com>
2026-02-19 22:12:52 +0100

    Leaderboard screen and responsive design done

--- frontend/levels/level.js
+++ frontend/levels/level.js
@@ -1,16 +1,18 @@
-
-
-const playerName = new URLSearchParams(window.location.search).get("playerName");
+const playerName = new URLSearchParams(window.location.search).get(
+  "playerName"
+);
 console.log("PLAYER NAME: ", playerName);
 
 const playerNameElement = document.getElementById("player-name");
-playerNameElement.textContent = playerName ? playerName+"!" : "Player!";
+playerNameElement.textContent = playerName ? playerName + "!" : "Player!";
 
 const links = document.querySelectorAll(".level a");
+const leaderboardLink = document.getElementById("leaderboard-page");
 
-links.forEach(link => { 
-    const level = link.getAttribute("href").split("level=")[1];
-    console.log("Setting link for level: ", level);
-    link.href = `../game/game.html?level=${level}&playerName=${(playerName)}`;
-})
+leaderboardLink.href = `../leaderBoard/leaderBoard.html?playerName=${playerName}`;
 
+links.forEach((link) => {
+  const level = link.getAttribute("href").split("level=")[1];
+  console.log("Setting link for level: ", level);
+  link.href = `../game/game.html?level=${level}&playerName=${playerName}`;
+});
